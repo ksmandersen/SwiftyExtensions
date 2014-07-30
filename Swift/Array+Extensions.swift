09407d18bc422dfb3675dd203bac65b715eb6640
Kristian Andersen <hello@kristian.co>
2014-07-30 11:34:49 +0200

    Added documentation to array extension

--- Swift/Array+Extensions.swift
+++ Swift/Array+Extensions.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 extension Array {
+	/// The first element in the array or nil if empty
 	var first: T? {
 		if isEmpty {
 			return nil
@@ -9,6 +10,7 @@ extension Array {
 		return self[startIndex]
 	}
 	
+	/// The last element in the array or nil if empty
 	var last: T? {
 		if isEmpty {
 			return nil
